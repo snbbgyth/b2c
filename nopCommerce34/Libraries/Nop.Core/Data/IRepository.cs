1e0b930099559d892e0a5b9628402d41f1610ac0
Spring Yang <snbbdx@sina.com>
2015-01-08 13:43:18 +0800

    add modify
    add modify

---
+++ nopCommerce34/Libraries/Nop.Core/Data/IRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace Nop.Core.Data
+{
+    /// <summary>
+    /// Repository
+    /// </summary>
+    public partial interface IRepository<T> where T : BaseEntity
+    {
+        /// <summary>
+        /// Get entity by identifier
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        T GetById(object id);
+
+        /// <summary>
+        /// Insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        void Insert(T entity);
+
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        void Update(T entity);
+
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        void Delete(T entity);
+
+        /// <summary>
+        /// Gets a table
+        /// </summary>
+        IQueryable<T> Table { get; }
+
+        /// <summary>
+        /// Gets a table with "no tracking" enabled (EF feature) Use it only when you load record(s) only for read-only operations
+        /// </summary>
+        IQueryable<T> TableNoTracking { get; }
+    }
+}
